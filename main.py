84a3d9fb4e258dd2f8fba0d48f7801f98baadf7c
jhoysbou <neeroll21Wgmail.com>
2019-12-28 01:03:36 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import cv2
-import numpy
-
-
-image = cv2.imread("./images/image_1.jpg")
-image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
-low = numpy.array((45, 40, 40), numpy.uint8)
-high = numpy.array((50, 250, 250), numpy.uint8)
-
-mask_grass = cv2.inRange(image,low, high)
-
-cv2.imshow("Original image", mask_grass) #
-cv2.waitKey(0)
+ # some stuff
